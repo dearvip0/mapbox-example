6b7ebb24cad3e4ce23707db6418b0a345f058662
dearvip0 <35306692+dearvip0@users.noreply.github.com>
2020-07-19 21:50:02 +0700

    init Map

---
+++ src/components/Header/Header.js
@@ -0,0 +1,119 @@
+import React from "react";
+import clsx from "clsx";
+import useStyles from "./styles";
+import { useTheme } from "@material-ui/core/styles";
+import {
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import Map from "../Map/Map";
+const Header = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {/* <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+        </Typography> */}
+        <Map />
+      </main>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Map/Map.js
@@ -0,0 +1,48 @@
+import React from "react";
+import mapboxgl from "mapbox-gl";
+import "./Map.css";
+import { Typography } from "@material-ui/core";
+class Map extends React.Component {
+  constructor() {
+    super();
+    this.mapRef = React.createRef();
+    this.state = {
+      lng: 106.65, //10.784328, 106.654547
+      lat: 10.78,
+      zoom: 13,
+    };
+  }
+
+  componentDidMount() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiZGVhcnZpcDAiLCJhIjoiY2p3cmx0Nm94MWlnNDQ4cGxndGJ2NDk5ayJ9.7RAd-OPulAnduD9tg4S5OA";
+    // create mapbox object
+    const map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Typography paragraph>
+        <div className="sidebarStyle">
+          Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+          {this.state.zoom}
+        </div>
+        <div style={{ width: "100%", height: "100vh" }} ref={this.mapRef}></div>
+      </Typography>
+    );
+  }
+}
+
+export default Map;
